3443e4b5ab9615e2db1b4edea9fad5842f9167fc
Jnaesi <jacobnasi@hotmail.com>
2022-10-25 17:32:11 +0000

    first commit

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://qtodppirupstbh:b5f6914ec10601aee330876eacf2b09a38a294d3a0f891dde163f637535907be@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dbgogc087v9ct9"
+os.environ["CLOUDINARY_URL"] = "cloudinary://546658217577574:JmORVr_Ka-Jq0mKM5aIeqjL5cz8@ddj06thqn"
+os.environ["SECRET_KEY"] = "lol123"
